ef7d0d964c646f2d4dc479c2a3152d4d4762889c
Finlay Ekins <140241456+f3kin@users.noreply.github.com>
2026-09-16 03:06:01 +0000

    feat(web): add master status board

--- apps/web/src/components/ChatView.tsx
+++ apps/web/src/components/ChatView.tsx
@@ -362,6 +362,7 @@ import type { AssistantCitationRequest } from "./chat/AssistantCitationSource";
 import { resolveTimelineIsAtEnd, worktreeSetupAgentStarted } from "./chat/MessagesTimeline.logic";
 import { resolveComposerTimelineInset, resolveScrollToEndClearance } from "./composerFooterLayout";
 import { ChatHeader } from "./chat/ChatHeader";
+import { MasterStatusBoard } from "./master/MasterStatusBoard";
 import { PanelLayoutControls, RightPanelMaximizeControl } from "./chat/PanelLayoutControls";
 import { expandedImageKey, type ExpandedImagePreview } from "./chat/ExpandedImagePreview";
 import { NoActiveThreadState } from "./NoActiveThreadState";
@@ -9524,6 +9525,8 @@ export default function ChatView(props: ChatViewProps) {
             onDeleteProjectScript={deleteProjectScript}
           />
         </WorkspacePageHeader>
+
+        <MasterStatusBoard activeThread={activeThread} />
 
         {/* Main content area with optional plan sidebar */}
         <div className="flex min-h-0 min-w-0 flex-1">
